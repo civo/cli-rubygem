9e818a4cbda8de14d23a5f15ae68141ce49b5e21
Kai <kai@Kaibook.local>
2019-06-10 10:40:37 +0100

    implement listing firewalls

--- lib/firewall.rb
+++ lib/firewall.rb
@@ -1,13 +1,26 @@
 module CivoCLI
   class Firewall < Thor
-    desc "", ""
-    def create
-
+    desc "create firewall_name", "create a new firewall"
+    def create(firewall_name)
+      # {ENV["CIVO_API_VERSION"] || "1"}/firewalls"
+      CivoCLI::Config.set_api_auth
+    
+      Civo::Firewall.create(name: firewall_name)
+      puts "        Created firewall #{firewall_name.colorize(:green)}"
+      
+      rescue Flexirest::HTTPException => e
+      puts e.result.reason.colorize(:red)
+      exit 1
     end
 
-    desc "", ""
+    desc "list", "lists all firewalls"
     def list
-
+      CivoCLI::Config.set_api_auth
+      rows = []
+      Civo::Firewall.all.items.each do |element|
+        rows << [element.id, element.name, element.rules_count, element.instances_count]
+      end
+      puts Terminal::Table.new headings: ['ID', 'Name', 'No. of Rules', 'instances using'], rows: rows
     end
 
     desc "", ""
