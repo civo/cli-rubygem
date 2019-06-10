85e008cf78f90ccf2fcd33381343e6b5bc953351
Kai <kai@Kaibook.local>
2019-06-10 10:12:14 +0100

    Create skeleton methods

--- lib/firewall.rb
+++ lib/firewall.rb
@@ -1,5 +1,33 @@
 module CivoCLI
   class Firewall < Thor
+    desc "", ""
+    def create
 
+    end
+
+    desc "", ""
+    def list
+
+    end
+
+    desc "", ""
+    def remove
+
+    end
+
+    desc "", ""
+    def new_rule
+
+    end
+
+    desc "", ""
+    def list_rules
+
+    end
+
+    desc "", ""
+    def delete_rule
+
+    end
   end
 end
