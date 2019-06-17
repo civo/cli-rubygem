5aa1e6dc38dd7b370fdcb7e461e36f902bb8d639
Kai Hoffman <notkaiho@gmail.com>
2019-06-17 16:51:04 +0100

    Linter recommendations
    Indenting/line spacing uniformity, whitespace on lines

--- lib/firewall.rb
+++ lib/firewall.rb
@@ -3,11 +3,10 @@ module CivoCLI
     desc "create firewall_name", "Create a new firewall"
     def create(firewall_name)
       CivoCLI::Config.set_api_auth
-    
+
       Civo::Firewall.create(name: firewall_name)
       puts "        Created firewall #{firewall_name.colorize(:green)}"
-      
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
@@ -20,8 +19,7 @@ module CivoCLI
         rows << [element.id, element.name, element.rules_count, element.instances_count]
       end
       puts Terminal::Table.new headings: ['ID', 'Name', 'No. of Rules', 'instances using'], rows: rows
-
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
@@ -29,22 +27,20 @@ module CivoCLI
     desc "remove firewall_ID", "Removes a firewall with Firewall ID provided"
     def remove(firewall_ID)
       CivoCLI::Config.set_api_auth
-      
       Civo::Firewall.remove(id: firewall_ID)
       puts "        Removed firewall #{firewall_ID.colorize(:red)}"
-
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
 
     desc "new_rule", "Create new rule on firewall firewall_name called rule_name with required arguments"
-    option :firewall_id, :required => true
-    option :protocol, :default => 'tcp'
-    option :start_port, :required => true
+    option :firewall_id, required: true
+    option :protocol, default: 'tcp'
+    option :start_port, required: true
     option :end_port
-    option :cidr, :default => '0.0.0.0/0'
-    option :direction, :default => 'inbound'
+    option :cidr, default: '0.0.0.0/0'
+    option :direction, default: 'inbound'
     option :label
     def new_rule
       CivoCLI::Config.set_api_auth
@@ -52,14 +48,11 @@ module CivoCLI
       if !options[:end_port]
         Civo::FirewallRule.create(firewall_id: options[:firewall_id], start_port: options[:start_port], end_port: options[:start_port], cidr: options[:cidr], direction: options[:direction], label: options[:label])
       end
-
       if options[:end_port]
         Civo::FirewallRule.create(firewall_id: options[:firewall_id], start_port: options[:start_port], end_port: options[:end_port], cidr: options[:cidr], direction: options[:direction], label: options[:label])
       end
-
       puts "        New rule" + (options[:label].nil? ? "" : " #{options[:label].colorize(:green)}") + " created"
- 
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
@@ -67,15 +60,13 @@ module CivoCLI
     desc "list_rules firewall_id", "Lists all active rules for firewall ID provided"
     def list_rules(firewall_id)
       CivoCLI::Config.set_api_auth
-      
       rules = Civo::FirewallRule.all(firewall_id: firewall_id)
       rows = []
       rules.each do |rule|
         rows << [rule.id, rule.protocol, rule.start_port, rule.end_port, rule.direction, rule.cidr.items.join(", "), rule.label]
       end
       puts Terminal::Table.new title: "Firewall rules for #{firewall_id}", headings: ['ID', 'Protocol', 'Start Port', 'End Port', 'Direction', 'CIDR', 'Label'], rows: rows
-      
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
@@ -86,8 +77,7 @@ module CivoCLI
 
       Civo::FirewallRule.remove(firewall_id: firewall_id, id: rule_id)
       puts "        Removed Firewall rule #{rule_id.colorize(:red)}"
-      
-      rescue Flexirest::HTTPException => e
+    rescue Flexirest::HTTPException => e
       puts e.result.reason.colorize(:red)
       exit 1
     end
