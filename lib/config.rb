1811a42ab8e23ba95faa8506b23efdfa79e9490d
Kai Hoffman <notkaiho@gmail.com>
2019-06-18 12:29:32 +0100

    Revert change to protect APIkey from deletion
    Follows from discussion on GitHub comments.

--- lib/config.rb
+++ lib/config.rb
@@ -26,7 +26,6 @@ module CivoCLI
     end
 
     def self.delete_apikey(key)
-      set_current_apikey_name(key)
       current["apikeys"].delete(key)
       if get_current_apikey_name == key
         if get_apikeys.any?
